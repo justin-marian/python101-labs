bae8874f0f032b71a41f13213f8e5fcdba68ce72
Justin Marian <pmarianjustin@gmail.com>
2025-02-15 17:26:50 +0200

    Delete Lab2 directory

--- Lab2/task_1/task01a.py
+++
@@ -1,9 +0,0 @@
-def task1a(nums):
-    '''
-    nums -> vector int
-    return -> vector int
-
-    Dublati elementele care se divid cu 6, iar pe cele 
-    care nu se divid, triplati-le folosind functionale
-    '''
-    return [x * 2 if x % 6 == 0 else x * 3 for x in nums]
